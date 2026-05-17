64a89646fd47ddb5b7bdcc28520e9f4227b34b10
S-umit-sharma <155601917+S-umit-sharma@users.noreply.github.com>
2026-05-17 20:33:41 +0530

    Library DTO is Ready

--- src/main/java/com/LMS/Library/Management/System/dto/LibraryDto.java
+++ src/main/java/com/LMS/Library/Management/System/dto/LibraryDto.java
@@ -9,12 +9,18 @@ import java.time.LocalTime;
 @Getter
 @Setter
 public class LibraryDto {
-    @NotBlank(message = "Enter the Description")
+    @NotBlank(message = "Enter the Description Of Your Library")
     private String details;
+    @NotBlank(message = "Enter the Website Url")
     private String website;
+    @NotBlank(message = "Enter the Library Opning Time")
     private LocalTime openingTime;
+    @NotBlank(message = "Enter the Library Clsoing Time")
     private LocalTime closingTime;
+    @NotBlank(message = "Enter the Book Issued Days")
     private Integer bookIssueDays;
+    @NotBlank(message = "Enter the Late Fine Amount")
     private Integer lateFine;
+    @NotBlank(message = "Enter the Desposite Amount")
     private Integer depositAmount;
 }
