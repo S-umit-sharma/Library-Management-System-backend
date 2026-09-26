cff54d4dfa5da8fcf22efd61b40c8d85f1831740
S-umit-sharma <155601917+S-umit-sharma@users.noreply.github.com>
2026-09-26 18:11:48 +0530

    new ci/cd with docker filw

--- src/main/java/com/LMS/Library/Management/System/controllers/UserController.java
+++ src/main/java/com/LMS/Library/Management/System/controllers/UserController.java
@@ -53,7 +53,7 @@ public class UserController {
         }
 
         return ResponseEntity.ok(new RegisterResponseDto(
-                "OTP sent successfullyyy",
+                "OTP sent successfully",
                 saveduser.getEmail()
         ));
     }
